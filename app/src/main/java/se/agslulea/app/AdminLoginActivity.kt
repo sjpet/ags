59f40d716570011c8a8ead18f159c409a4a12ece
Stefan Peterson <stefan.peterson@rubico.com>
2017-08-31 15:23:19 +0200

    design improvements

---
+++ app/src/main/java/se/agslulea/app/AdminLoginActivity.kt
@@ -0,0 +1,12 @@
+package se.agslulea.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_login)
+    }
+}
